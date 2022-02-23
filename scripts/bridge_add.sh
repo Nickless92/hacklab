53eb484e8d9f6bcda0230310ae4394a223c9c4a3
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-02-23 15:42:04 +0000

    clean up

--- scripts/bridge_add.sh
+++ scripts/bridge_add.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-
-<<<<<<< Updated upstream
-#sudo apt-get install brctl         # first, make sure brctl is installed
-
-sudo brctl addbr $1 up              # adds and starts a bridge named $1 
-sudo ip link set dev $1 up          # adds and starts the L2 link
-echo "created bridge for level $1"
-
-=======
-<<<<<<< Updated upstream
-sudo brctl addbr $1 up
-sudo ip link set dev $1 up
-echo "created $1"
-=======
 # $1 = level
 
-#sudo apt-get install brctl         # first, make sure brctl is installed
 if [ $# = 1 ]
     sudo brctl addbr $1 up              # adds and starts a bridge named $1 
     sudo ip link set dev $1 up          # adds and starts the L2 link
@@ -23,5 +8,3 @@ if [ $# = 1 ]
 else
     echo "[bridge_add] invalid number of parameters"
 fi
->>>>>>> Stashed changes
->>>>>>> Stashed changes
