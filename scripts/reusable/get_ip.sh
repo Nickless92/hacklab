510bb47f3b285a7039abafd845196e83d1c4aa5a
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-02-24 16:16:51 +0000

    cleanup

--- scripts/reusable/get_ip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# script to get a container's own IP
-
-ip a | grep "global eth0" | sed 's/    inet //' | sed 's/\/.*//' > my_ip.txt
---
+++ scripts/reusable/tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# script to init layout for level 01
+
+./start_level01.sh >> container01.log 2>&1 | tmux
