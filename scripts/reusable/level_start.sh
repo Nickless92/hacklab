9f433fe805929562ecf05ffe1e947da435562124
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-02-25 16:15:44 +0000

    fix path in doninic_menu.c

--- scripts/reusable/level_start.sh
+++ scripts/reusable/level_start.sh
@@ -6,15 +6,16 @@
 
 LOGFILE=$(echo "$0" | sed s/'.sh'/'.log'/)
 echo -n "*** " >> "$LOGFILE"; date=$(date); echo -n "$date" >> "$LOGFILE"; echo " ***" >> "$LOGFILE"
-echo "level = $1 - devices = $2" &>> "$LOGFILE" 
+echo "level = $1 - devices = $2" &>> "$LOGFILE"
+path=$(pwd); echo "path=$path" &>> "$LOGFILE"
 
 # to do: check for more complex calls (number of parameters)
 if [ "$#" -eq 2 ]
 then
-    sudo ./bridge_add.sh "$1"; pause &>> "$LOGFILE"
-    sudo ./container_create.sh "$1" "$2"; pause &>> "$LOGFILE"
-    sudo ./container_start.sh "$1" "$2"; pause &>> "$LOGFILE"
-    sudo ./container_ip_add.sh "$1" "$2"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/bridge_add.sh "$1"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/container_create.sh "$1" "$2"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/container_start.sh "$1" "$2"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/container_ip_add.sh "$1" "$2"; pause &>> "$LOGFILE"
     echo "[level_start] created level $1 with $2 devices" &>> "$LOGFILE"
 else
     echo "[level_start] invalid number of parameters" &>> "$LOGFILE"
--- scripts/reusable/level_stop.sh
+++ scripts/reusable/level_stop.sh
@@ -7,14 +7,15 @@
 LOGFILE=$(echo "$0" | sed s/'.sh'/'.log'/)
 echo -n "*** " >> "$LOGFILE"; date=$(date); echo -n "$date" >> "$LOGFILE"; echo " ***" >> "$LOGFILE"
 echo "level = $1 - devices = $2" &>> "$LOGFILE" 
+path=$(pwd); echo "path=$path" &>> "$LOGFILE"
 
 # to do: check for more complex calls (number of parameters)
 if [ "$#" -eq 2 ]
 then
-    sudo ./container_ip_del.sh "$1" "$2" &>> "$LOGFILE"; pause &>> "$LOGFILE"
-    sudo ./container_stop.sh "$1" "$2" &>> "$LOGFILE"; pause &>> "$LOGFILE"
-    sudo ./container_delete.sh "$1" "$2" &>> "$LOGFILE"; pause &>> "$LOGFILE"
-    sudo ./bridge_del.sh "$1" &>> "$LOGFILE"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/container_ip_del.sh "$1" "$2" &>> "$LOGFILE"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/container_stop.sh "$1" "$2" &>> "$LOGFILE"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/container_delete.sh "$1" "$2" &>> "$LOGFILE"; pause &>> "$LOGFILE"
+    sudo $path/../scripts/reusable/bridge_del.sh "$1" &>> "$LOGFILE"; pause &>> "$LOGFILE"
     echo "[level_start] destroyed level $1 with $2 devices" &>> "$LOGFILE"
 else
     echo "[level_start] invalid number of parameters" &>> "$LOGFILE"
