2b0a0145ff432bc69facdd8af2174d79f37ca21d
Jean-Arole Fotsing <inf3608@hs-worms.de>
2022-02-28 17:01:48 +0100

    with header

---
+++ c-programs/Name.h
@@ -0,0 +1,50 @@
+#ifndef NAME_H_INCLUDED
+#define NAME_H_INCLUDED
+
+	void name()
+{
+    initscr();                                                                  //initialize the screen
+    noecho();                                                                   //suppresses to show the symbols you type in                                          
+    //cbreak();                                                                   //disables line buffering
+    curs_set(0);                                                                //disables the curser
+    
+    int yMAx, xMAx = 0;                                                         //variables for the size of the screen
+    getmaxyx(stdscr, yMAx, xMAx);                                               //function to get the maximum size of the screen of your comput
+    
+    WINDOW * namewin = newwin(6, xMAx - 12, yMAx - 8, 5);                       //WINDOW creates a new window
+    box(namewin, 0, 0);  
+    mvwprintw(namewin, 1, 1, "Please enter a name: ");                          //prints text in the box
+    mvwprintw(namewin, 4, xMAx - 25, "EXIT [ESC]");
+    refresh();                                                                  //you have to refresh the screen that you can see the new text
+    wrefresh(namewin);                                                          //refreshes the window
+    char username[] = {};                                                       //char array to save the name the user entered
+    int position_of_current_letter = 21;                                        //we need this, because the name the user entered should be printed next to th string "Please enter a name: "
+    
+    
+    do
+    {
+        if(username[position_of_current_letter - 21] == 127)                    //127 describes the delete character in Ascii
+        {
+            //goto EINGABE;
+
+            //how can we delete the last character???????
+        }
+        else if(username[position_of_current_letter - 21] == 27)                //27 describes the ESC character in Ascii
+        {
+            endwin();                                                           //closes the current window 
+            exit(0);                                                            //and we return to the terminal again
+        }
+        else 
+        {       
+            mvwprintw(namewin, 1, ++position_of_current_letter, "%c", username[position_of_current_letter - 21]);             //each letter will be printed in the box/Window "namewin"
+        }
+        wrefresh(namewin);                                                      //you need to refresh the window, that the name the user types in is shown   
+    } while((username[position_of_current_letter - 21] = getch()) != '\n');
+    
+   menu();
+    
+
+}
+
+#endif
+	
