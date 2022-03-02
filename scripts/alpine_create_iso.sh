3e9762d3a41cbd945d9ad7b6870045a402086211
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-02 22:50:20 +0100

    streamline names schema: lost+found

--- scripts/alpine_create_iso.sh
+++ scripts/alpine_create_iso.sh
@@ -7,8 +7,9 @@ LOGFILE=$(echo "$0" | sed s\#'.sh'\#'.log'\# | sed s\#'^.*/'\#'/var/log/hacklab/
 
 # first, make sure to delete an existing alpine-runner container
 sudo lxc delete alpine-runner --force
-# second, make sure to delete an existing alpine-iso-stage image
-sudo lxc image delete alpine-iso-stage
+# second, make sure to delete existing iso-alpine-stage/-edge images
+sudo lxc image delete iso-alpine-stage
+sudo lxc image delete iso-alpine-edge
 echo "[$0] $(date) - DONE: purged leftovers"
 
 # create container alpine-runner with image alpine/edge
@@ -19,11 +20,6 @@ echo "[$0] $(date) - DONE: started alpine-runner with alpine/edge image"
 
 # config lxdbr0 to provide internet access via DHCP
 sudo lxc network set lxdbr0 ipv4.dhcp=true
-
-# add internet - unnecessary due to bridge config
-#sudo lxc config device add alpine-runner eth0 nic nictype=bridged parent=lxdbr0 name="eth0"
-#sudo lxc exec alpine-runner -- ip addr add 10.0.0.1/24 dev "eth0"
-#sudo lxc exec alpine-runner -- ip link set dev "eth0" up
 
 # install packages
 sudo lxc file push ./alpine_install_tools.sh alpine-runner/root/install_tools.sh
@@ -39,9 +35,9 @@ echo "[$0] $(date) - DONE: snapshot + publishing as ISO"
 # check if staging alpine-utils succeeded - if yes, overwrite iso-alpine-utils
 if [ $(sudo lxc image show iso-alpine-stage) -eq 0 ]
 then
-    echo "[$0] $(date) - DONE: succesful test for iso-alpine-stage"
+    echo "[$0] $(date) - DONE: iso-alpine-stage successfully created"
     sudo lxc image delete iso-alpine-utils
-    echo "[$0] $(date) - DONE: purged iso-alpine-utils"
+    echo "[$0] $(date) - DONE: purged stale iso-alpine-utils"
     sudo lxc image alias rename iso-alpine-stage iso-alpine-utils
     echo "[$0] $(date) - DONE: renamed in iso-alpine-utils - READY!"
 else
