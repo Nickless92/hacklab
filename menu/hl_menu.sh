d569fb073dc685b9d3fb5f83d65d91517e017578
Dominic Meyer <inf3644@hs-worms.de>
2022-03-02 16:27:42 +0000

    added returns to functions

--- menu/hl_menu.sh
+++ menu/hl_menu.sh
@@ -2,18 +2,18 @@ function module_menu () {
     echo -ne "
     Please choose a module: 
     $(ColorBlue '1)') Networks (german: Netzwerke)
-	$(ColorBlue '2)') Network Security
+    $(ColorBlue '2)') Network Security
     $(ColorRed 'back)') Back to previous menu
-	$(ColorRed 'esc)') Exit
+    $(ColorRed 'esc)') Exit
 	Choose a module: 
 	"
     read menuinput
     case $menuinput in
-    1) echo "you chose networks." ; networks_levels ;;
-    2) echo "you chose networks." ; netsec_levels ;; 
-    back) menu ;; 
+    1) echo "you chose networks." ; return 5 ;;
+    2) echo "you chose networks." ; return 6 ;; 
+    back) return 2 ;; 
     esc) exit 0 ;;
-    *) echo -e $red"Wrong option."$clear; menu;;
+    *) echo -e $red"Wrong option."$clear; return 4;;
     esac
 
 }
@@ -32,14 +32,14 @@ function netsec_levels () {
 	"
     read menuinput
     case $menuinput in
-    1) echo "you chose level 1. this is empty." ; netsec_levels ;;
-	2) echo "you chose level 2. this is empty." ; netsec_levels ;;
-	3) echo "you chose level 3. this is empty." ; netsec_levels ;;
-	4) echo "you chose level 4. this is empty." ; netsec_levels ;; #add shell script to levels, then menu
-	5) echo "you chose level 5. this is empty." ; netsec_levels ;;
-    back) module_menu ;; 
+    1) echo "you chose level 1. this is empty." ;  return 6;;
+	2) echo "you chose level 2. this is empty." ;  return 6;;
+	3) echo "you chose level 3. this is empty." ;  return 6;;
+	4) echo "you chose level 4. this is empty." ;  return 6;; #add shell script to levels, then menu
+	5) echo "you chose level 5. this is empty." ;  return 6;;
+    back) return 4 ;; 
     esc) exit 0 ;;
-    *) echo -e $red"Wrong option."$clear; menu;;
+    *) echo -e $red"Wrong option."$clear; return 6;;
     esac
 }
 
@@ -57,14 +57,14 @@ function networks_levels () {
 	"
     read menuinput
     case $menuinput in
-    1) echo "you chose level 1. starting..." ; ../scripts/level_start.sh 1 3 ; clear ; sudo tmux new-session -s lvl01 -d 'lxc exec lvl01-d02 -- tshark -i eno1'';' split-window -v -d 'lxc shell lvl01-d01' ';' split-window -h -d 'lxc exec lvl01-d03 -- tshark -i eno1' ';' attach; networks_levels ;;
-	2) echo "you chose level 2" ; networks_levels ;;
-	3) echo "you chose level 3" ; networks_levels ;;
-	4) echo "you chose level 4" ; networks_levels ;; #add shell script to levels, then menu
-	5) echo "you chose level 5" ; networks_levels ;;
-    back) module_menu ;; 
+    1) echo "you chose level 1. starting..." ; ../scripts/level_start.sh 1 3 ; clear ; sudo tmux new-session -s lvl01 -d 'lxc exec lvl01-d02 -- tshark -i eno1'';' split-window -v -d 'lxc shell lvl01-d01' ';' split-window -h -d 'lxc exec lvl01-d03 -- tshark -i eno1' ';' attach; return 5 ;;
+	2) echo "you chose level 2" ; return 5 ;;
+	3) echo "you chose level 3" ; return 5 ;;
+	4) echo "you chose level 4" ; return 5 ;; #add shell script to levels, then menu
+	5) echo "you chose level 5" ; return 5 ;;
+    back) return 4 ;; 
     esc) exit 0 ;;
-    *) echo -e $red"Wrong option."$clear; menu;;
+    *) echo -e $red"Wrong option."$clear; return 5;;
     esac
 }
 
@@ -81,42 +81,45 @@ function pupil_level () {
 	"
     read menuinput
     case $menuinput in
-    1) echo "you chose level 1. starting..." ; ../scripts/level_start.sh 1 3 ; clear ; sudo tmux new-session -s lvl01 -d 'lxc exec lvl01-d02 -- tshark -i eno1'';' split-window -v -d 'lxc shell lvl01-d01' ';' split-window -h -d 'lxc exec lvl01-d03 -- tshark -i eno1' ';' attach; pupil_level ;;
-	2) echo "you chose level 2" ; menu ;;
-	3) echo "you chose level 3" ; menu ;;
-	4) echo "you chose level 4" ; menu ;; #add shell script to levels, then menu
-	5) echo "you chose level 5" ; menu ;;
-    back) menu ;; 
+    1) echo "you chose level 1. starting..." ; ../scripts/level_start.sh 1 3 ; clear ; sudo tmux new-session -s lvl01 -d 'lxc exec lvl01-d02 -- tshark -i eno1'';' split-window -v -d 'lxc shell lvl01-d01' ';' split-window -h -d 'lxc exec lvl01-d03 -- tshark -i eno1' ';' attach; return 3 ;;
+	2) echo "you chose level 2" ; return 3 ;;
+	3) echo "you chose level 3" ; return 3 ;;
+	4) echo "you chose level 4" ; return 3 ;; #add shell script to levels, then menu
+	5) echo "you chose level 5" ; return 3 ;;
+    back) return 2 ;; 
     esc) exit 0 ;;
-    *) echo -e $red"Wrong option."$clear; menu;;
+    *) echo -e $red"Wrong option."$clear; return 3;;
     esac
 }
 
 function choose_stage () {
+    #echo -e "Welcome to our hacklab $(ColorGreen $name)!\nThis is a game in which you can start learning how to manipulate networks and defend yourself from cyber attacks.\nTo start, we will need to know how much knowledge you already posess.\nPlease choose one of the following options:\n"
+
     echo -ne "
-    $(ColorBlue '1)') Pupil  -  for people who have not attended any modules at HS-Worms
-    $(ColorBlue '2)') Student  -  in the next step you can choose between different modules
-    $(ColorBlue '3)') Expert  -  you can choose between all levels of pupil and student
+    $(ColorBlue '1)') Pupil     -  for people who have not attended any modules at HS-Worms
+    $(ColorBlue '2)') Student   -  in the next step you can choose between different modules
+    $(ColorBlue '3)') Expert    -  you can choose between all levels of pupil and student
     $(ColorRed 'esc)') Exit
     "
 
     read menuinput
     case $menuinput in 
-	1) echo "you have chosen pupil." ; pupil_level ; menu ;; 
-	2) echo "you have chosen student." ; module_menu ; menu ;; 
+	1) echo "you have chosen pupil." ; return 3 ;; 
+	2) echo "you have chosen student." ; return 4 ;; 
 	3) echo "you have chosen expert." ; menu ;; 
     esc) exit 0 ;; 
-    *) echo -e $red"Wrong option."$clear; menu;;
+    *) echo -e $red"Wrong option."$clear; return 2;;
     esac
 }
 
 function enter_name(){
     echo "Please enter your name, enter esc to exit"
     read name
-    if [ $name == "esc" ] ; then
-	exit 0 ; 
-    fi
-    
+    if [ $name = "esc" ] ; then
+	    return 0
+    else
+        return 2
+    fi 
 }
 
 
@@ -140,13 +143,28 @@ ColorRed(){
 }
 
 
-menu () {
-    
-    choose_stage
+menu () 
+{
+    enter_name
+    echo -e "Welcome to our hacklab $(ColorGreen $name)!\nThis is a game in which you can start learning how to manipulate networks and defend yourself from cyber attacks.\nTo start, we will need to know how much knowledge you already posess.\nPlease choose one of the following options:\n"
+    returnValue=2
 
+    while : ; do
+        case $returnValue in
+            1) enter_name;;
+            2) choose_stage;;
+            3) pupil_level;;
+            4) module_menu;;
+            5) networks_levels;;
+            6) netsec_levels;;
+            *) exit 0;;
+        esac
+        returnValue=$?
+        [[ $returnValue -ne 0 ]] || break
+    done
 }
 
 #call menu function 
-enter_name
-echo -e "Welcome to our hacklab $(ColorGreen $name)!\nThis is a game in which you can start learning how to manipulate networks and defend yourself from cyber attacks.\nTo start, we will need to know how much knowledge you already posess.\nPlease choose one of the following options:\n"
+#enter_name
+#echo -e "Welcome to our hacklab $(ColorGreen $name)!\nThis is a game in which you can start learning how to manipulate networks and defend yourself from cyber attacks.\nTo start, we will need to know how much knowledge you already posess.\nPlease choose one of the following options:\n"
 menu
