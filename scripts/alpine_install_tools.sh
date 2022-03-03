427b70092428befe2616e89ae7cfa2b057379a46
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-03 14:34:42 +0100

    add man support

--- scripts/alpine_install_tools.sh
+++ scripts/alpine_install_tools.sh
@@ -10,6 +10,11 @@ apk update
 apk upgrade
 echo "[$(date)] done: update"
 
+# install man
+echo "[$(date)] todo: man"
+apk add mandoc
+echo "[$(date)] done: man"
+
 # install git
 echo "[$(date)] todo: git"
 apk add git
@@ -31,46 +36,61 @@ echo "[$(date)] done: git"
 # install MACchanger
 echo "[$(date)] todo: macchanger"
 apk add macchanger
+apk add macchanger-doc
 echo "[$(date)] done: macchanger"
 
 # install DNSrecon
 echo "[$(date)] todo: dnsrecon"
 apk add dnsrecon
+# didn't find any docu sources in repo
 echo "[$(date)] done: dnsrecon"
 
 # install nmap
 echo "[$(date)] todo: nmap"
 apk add nmap
+apk add nnmap-doc
 echo "[$(date)] done: nmap"
 
 # install tmux
 echo "[$(date)] todo: tmux"
 apk add tmux
+apk add tmux-doc
 echo "[$(date)] done: tmux"
+
+# install iptables
+echo "[$(date)] todo: iptables"
+apk add iptables
+apk add iptables-doc
+echo "[$(date)] done: iptables"
 
 # install tshark
 echo "[$(date)] todo: tshark"
 apk add tshark
+apk add wireshark-doc
 echo "[$(date)] done: tshark"
 
 # try to install hping3 from main repo first
 echo "[$(date)] todo: hping3"
 apk add hping3
+apk add hping3-doc
 echo "[$(date)] done: hping3"
 
 # install hping3 from edge/testing repo
 echo "[$(date)] todo: hping3 (testing)"
 apk add -X http://dl-cdn.alpinelinux.org/alpine/edge/testing hping3
+apk add -X http://dl-cdn.alpinelinux.org/alpine/edge/testing hping3-doc
 echo "[$(date)] done: hping3 (testing)"
 
 # try to install ettercap from main repo first
 echo "[$(date)] todo: ettercap"
 apk add ettercap
+apk add ettercap-doc
 echo "[$(date)] done: ettercap"
 
 # install ettercap from edge/testing repo
 echo "[$(date)] todo: ettercap (testing)"
 apk add -X http://dl-cdn.alpinelinux.org/alpine/edge/testing ettercap
+apk add -X http://dl-cdn.alpinelinux.org/alpine/edge/testing ettercap-doc
 echo "[$(date)] done: ettercap (testing)"
 
 echo "[$(date)] DONE"
