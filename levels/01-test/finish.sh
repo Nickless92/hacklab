4e9ee2d45a0261ba5ae10a1db2e3677d4b61ceb2
Thierry <inf3494@hs-worms.de>
2022-03-03 12:03:32 +0100

    comment file finish.sh

--- levels/01-test/finish.sh
+++ levels/01-test/finish.sh
@@ -13,45 +13,32 @@
 
 #script to determine if the attack (Ip-Spoofing) was successful
 
-#to control the logs from the victim in tshark
-#sudo lxc exec test2 -- tshark -c 7 > log.txt
 
-#sudo lxc exec lvl01-d01 -- cat ./.ash_history | tail -n2 > command.txt | head -n1
+#to obtain the file where the User saves his command(filename: command.txt)
 sudo lxc file pull lvl01-d01/root/command.txt ./command.txt
 
-result=$(diff ../levels/01-test/test.txt command.txt | wc -l)
-#command to do the attack
+#result=$(diff ../levels/01-test/test.txt command.txt | wc -l)
 #file=$(cat command.txt)
 #sudo lxc file push ./command.sh test1/root/command.sh
-#sudo lxc exec test1 -- sh command.sh
-#sudo lxc exec test2 -- tshark -c 7 > log.txt
-#sudo lxc exec test1 -- sh command.sh
-
-#lxc exec VM1 -- ping -c5 8.8.8.8 > log.txt
 
 
-#to determine how many request and reply receive the victim
-#reply=$(grep reply log.txt | wc -l)
-#request=$(grep request log.txt | wc -l)
-#variable=$(cat command.txt)
-#echo "$variable"
 
+#to save all the parameters of the sample solution in variables containing only numbers
 test1=$(grep -c "\-1" command.txt)
 test2=$(grep -c "\-a 10.10.1.2 10.10.1.3" command.txt)
 test3=$(grep -c "\-c 7" command.txt)
 test4=$(grep -c "\-p 80" command.txt)
 test5=$(grep "hping3" command.txt | sed s/" .*"//)
 
+
 #test to determine if the attack of the user was successful
+#to determine if all the parameters of the sample solution are included in the users'command
 if [ "$test1" = 1 ] && [ "$test2" = 1 ] && [ "$test3" = 1 ] && [ "$test4" = 1 ] && [ "$test5" = "hping3" ]
 then 
-cat ../ressources/ascii/win.txt
+    # then the user won 
+    cat ../ressources/ascii/win.txt
 else 
-cat ../ressources/ascii/lost.txt
+    #then the user lost 
+    cat ../ressources/ascii/lost.txt
 fi
 
-#to know if the user was successful
-if [ "$result" = 0 ]
-then 
-echo The attack was successful
-fi
