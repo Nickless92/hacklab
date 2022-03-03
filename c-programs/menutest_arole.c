70d00e720960e1a1864d661ffe62b13af47fa513
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-03 16:28:34 +0100

    change: add license to .c programs

--- c-programs/menutest_arole.c
+++ c-programs/menutest_arole.c
@@ -1,3 +1,14 @@
+// SPDX-FileCopyrightText: 2022 Bassam-Khaled Thiab <inf3891@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Dominic Meyer <inf3644@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Jean-Arole Fotsing <inf3608@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Katrin Vollhardt <inf3493@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Steve Landry Tchamabe <infXXXX@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Syed Saad-Ullah <inf3664@hs-worms.de>
+// SPDX-FileCopyrightText: 2022 Thierry-Junior Djeutchou-Njieyong <inf3494@hs-worms.de>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <stdio.h>
 #include <ncurses.h>
 #include <string.h>
