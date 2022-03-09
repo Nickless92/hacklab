1d00aff6f0c7bba0ab6a20d0f26187bda7638f1b
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-09 14:21:22 +0100

    change: major overhaul of project directories

--- scripts/test/logs_delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# script to remove log files
-
-rm -f *.log
-rm -ri ./logs/
-rm -ri /var/log/hacklab/
-echo "All log files deleted! =)"
--- scripts/test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# print everything into ./logs/SCRIPT.log
-LOGFILE=$(echo "$0" | sed s\#'.sh$'\#'.log'\# | sed s\#'^./'\#'./logs/'\# ); exec &>> "$LOGFILE"
-echo "[$0] *** $(date) ***"; echo "[$0] level = $1 - devices = $2"
