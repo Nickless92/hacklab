b607678904e625d43d334ee233111d1104e39b68
Thierry <inf3494@hs-worms.de>
2022-03-09 03:14:15 +0100

    change file finish.sh

--- levels/01-test/create_level_session.sh
+++ levels/01-test/create_level_session.sh
@@ -2,7 +2,7 @@
 #script to start a tmux-session and all windows for the level01 
 
 #create the  session and two split pane
-tmux new-session -s hacklab -n level01 -d 'lxc exec lvl01-c02 -- tshark -i eth0; bash -i'\; split-window -v -d 'lxc shell lvl01-c01'\; split-window -h -d 'lxc exec lvl01-c03 -- tshark -i eth0' \; select-pane -D
+tmux new-session -s hacklab -n level01 -d 'lxc exec lvl01-c02 -- tshark -i eth0 > ausgabe.txt; bash -i'\; split-window -v -d 'lxc shell lvl01-c01'\; split-window -h -d 'lxc exec lvl01-c03 -- tshark -i eth0' \; select-pane -D
 
 #create a new window and run 
 tmux new-window -t hacklab:1 -n help 'ls -l; bash -i'
