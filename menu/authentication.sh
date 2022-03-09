dc36520efc28f1fa6b8f10a7ead94509f69855fd
Dominic Meyer <inf3644@hs-worms.de>
2022-03-09 15:22:27 +0100

    added functions

--- menu/authentication.sh
+++ menu/authentication.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
+function generateCode()
+{
+	RANDOM=$$
+	for i in `seq 1`
+	do
+		hash=$(echo $RANDOM | sha256sum | cut -c1-6)
+	done	
+}
 
-RANDOM=$$
+function enterEmail()
+{
+	echo -n "Bitte geben Sie Ihre E-mail Adresse ein: "
+	read email
+	echo -n "Ist folgende E-Mail korrekt $email ? [J/N]: "
+	read -n 2 answer
+	case $answer in
+		J|j) 
+			echo -e "Dein Code lautet: \n$hash" | mail -s "Dein Zugangscode für Hacklab" $email ;
+			echo -e "Sending E-Mail...\n";;
+		N|n) enterEmail;;
+		*) echo -e "Wrong option" ; enterEmail;;
+	esac
+}
 
-#zufall=$RANDOM
+function enterCode()
+{
+	for ((i=1; i<=3; i++));
+	do
+		echo -n "($i. Versuch/3) Bitte geben Sie hier den Code ein, den Sie per E-Mail erhalten haben: "
+		read -n 6 eingabe
+		if [ "$eingabe" == "$hash" ]; then
+				echo -e "\nrichtig"
+			i=4
+		else
+				echo -e "\nleider falsch"
+		fi
+	done
+}
 
-for i in `seq 1`
-do
-
-	echo $RANDOM > data
-	#cat data
-	hash=$(sha256sum data | cut -c1-6)
-	#echo $hash
-done
-echo -n "Bitte geben Sie Ihre E-mail Adresse ein: "
-read email
-echo -e "Dein Code lautet: \n$hash" | mail -s "Dein Zugangscode für Hacklab" $email
-
-for ((i=1; i<=3; i++));
-do
-	echo -n "($i. Versuch/3) Bitte geben Sie hier den Code ein, den Sie per E-Mail erhalten haben: "
-	read -n 6 eingabe
-	if [ "$eingabe" == "$hash" ]; then
-    		echo -e "\nrichtig"
-		i=4
-	else
-    		echo -e "\nleider falsch"
-	fi
-done
+generateCode
+enterEmail
+enterCode
