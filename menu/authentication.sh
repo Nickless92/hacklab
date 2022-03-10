3159f9cc9b9d0fb5b7c0f04f1f318de7a74d84d8
Dominic Meyer <inf3644@hs-worms.de>
2022-03-10 13:43:39 +0100

    added regular expression

--- menu/authentication.sh
+++ menu/authentication.sh
@@ -21,17 +21,22 @@ function enterEmail()
 {
 	echo -n "Bitte geben Sie Ihren InfXXXX Namen ein: "
 	read infName
-	email=${infName}@hs-worms.de																		#infName + @hs-worms.de --> E-Mail Adress
-	echo -n "Ist folgende E-Mail korrekt $email ? [J/N]: "
-	read -n 2 answer																					
-	case $answer in																						#checks the enter value 
-		J|j) 
-			echo -e "Sending E-Mail...\n";
-			generateCode			
-			echo -e "Dein Code lautet: \n$hash\nDieser läuft in $limitInMinutes Minuten ab" | mail -s "Dein Zugangscode für Hacklab" -a FROM:"Team Hacklab <teamhacklab@gmail.com>" $email ;;	#generates Mail Packet
-		N|n) enterEmail;;																				#the user can enter his/her email again
-		*) echo -e "Wrong option" ; enterEmail;;
-	esac
+	if [[ "$infName" =~ ^inf[0-9]{4}$ ]] ; then															#checks if the user puts in a valid Adress
+		email=${infName}@hs-worms.de																	#infName + @hs-worms.de --> E-Mail Adress
+		echo -n "Ist folgende E-Mail korrekt $email ? [J/N]: "
+		read -n 2 answer																					
+		case $answer in																					#checks the enter value 
+			J|j) 
+				echo -e "Sending E-Mail...\n";
+				generateCode			
+				echo -e "Dein Code lautet: \n$hash\nDieser läuft in $limitInMinutes Minuten ab" | mail -s "Dein Zugangscode für Hacklab" -a FROM:"Team Hacklab <teamhacklab@gmail.com>" $email ;;	#generates Mail Packet
+			N|n) enterEmail;;																			#the user can enter his/her email again
+			*) echo -e "Wrong option" ; enterEmail;;
+		esac
+	else
+		echo -e "Kein gültiger inf-Name\n"
+		enterEmail
+	fi
 }
 
 function enterCode()
