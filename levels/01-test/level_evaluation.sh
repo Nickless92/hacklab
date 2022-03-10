42a974b06f9c6403d7f03ea0c4a0e5633c82f1ca
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-10 01:41:39 +0100

    change: extract functions from level01, put them in hacklab/scripts/ directory

--- levels/01-test/level_evaluation.sh
+++ levels/01-test/level_evaluation.sh
@@ -14,18 +14,18 @@
 # determine if the attack (ip spoofing) was successful
 
 # to obtain the file where the User saves his command(filename: command.txt)
-lxc file pull lvl01-c01/root/command.txt ../levels/01-test/command.txt
+lxc file pull lvl01-c01/root/command.txt ../levels/01-test/command_from_user.txt
 
-#result=$(diff ../levels/01-test/test.txt command.txt | wc -l)
-#file=$(cat command.txt)
-#lxc file push ./command.sh test1/root/command.sh
+#result=$(diff ../levels/01-test/test.txt command_from_user.txt | wc -l)
+#file=$(cat command_from_user.txt)
+#lxc file push ./command_from_user.sh test1/root/command.sh
 
 # to save all the parameters of the sample solution in variables containing only numbers
-test1=$(grep -c "\-1" ../levels/01-test/command.txt)
-test2=$(grep -c "\-a 10.10.1.2 10.10.1.3" ../levels/01-test/command.txt)
-test3=$(grep -c "\-c 7" ../levels/01-test/command.txt)
-test4=$(grep -c "\-p 80" ../levels/01-test/command.txt)
-test5=$(grep "hping3" ../levels/01-test/command.txt | sed s/" .*"//)
+test1=$(grep -c "\-1" ../levels/01-test/command_from_user.txt)
+test2=$(grep -c "\-a 10.10.1.2 10.10.1.3" ../levels/01-test/command_from_user.txt)
+test3=$(grep -c "\-c 7" ../levels/01-test/command_from_user.txt)
+test4=$(grep -c "\-p 80" ../levels/01-test/command_from_user.txt)
+test5=$(grep "hping3" ../levels/01-test/command_from_user.txt | sed s/" .*"//)
 
 # test to determine if the attack of the user was successful
 # to determine if all the parameters of the sample solution are included in the users'command
@@ -34,6 +34,6 @@ then
     # then the user won 
     cat ../ressources/ascii/win.txt
 else 
-    #then the user lost 
+    # then the user lost 
     cat ../ressources/ascii/lost.txt
 fi
