4c5ae1d52f987ac2620296b48601ae4a875f0ce1
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-14 10:13:22 +0100

    add: print_paragraph function
    $1: paragraph ID, $2 pause time, $3 path/to/file
    prints a whole paragraph from file ($3),
    starting at <ID> ($1), ends at ===
    (make sure to put each tag in seperate lines)
    passes pause time ($2) to print_lines_from_file_then_pause()

---
+++ scripts/print_paragraph.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# tl;dr: retrieve paragraph from file and call print_lines_from_file_then_pause()
+# prints a whole paragraph from file ($3), passes on pause time ($2)
+# starts at '<ID>', ends at '===' (make sure to put each tag in seperate lines)
+
+# $1: paragraph ID
+# $2: pause time    (passed on to print_lines_from_file_then_pause)
+# $3: path/to/file  (passed on to print_lines_from_file_then_pause)
+
+file=$(readlink -f "$3")                                            # get path to file
+startline=$(( $( grep -n "<$1>" $file | sed s/':.*'/''/ ) + 1 ))    # extract line where <TAG> appears
+
+while read -r match; do                                             # loop over every match from subproc (line13)
+    line=$(echo $match | sed s/':.*'//)                             # extract line where '===' appeared
+    [ $line -gt $startline ] && endline=$(( $line - 1 )); break     # search for first '===' line after <TAG>
+done < <(grep -n === $file)                                         # pipe from (subproc) into while loop
+                                                                    # subproc: greps for '===' lines in $file
+
+./scripts/print_lines_from_file_then_pause $startline $endline $2 $3    # FIXME - check if path to script doesn't break
