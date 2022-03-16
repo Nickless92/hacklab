f9b459c5df99baa92059b7ae3b6355c84aaebdb4
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-16 02:42:53 +0100

    change: better documentation/comments - scripts + refactor

--- scripts/print_paragraph.sh
+++ scripts/print_paragraph.sh
@@ -8,13 +8,13 @@
 # $2: pause time    (passed on to print_lines_from_file_then_pause)
 # $3: path/to/file  (passed on to print_lines_from_file_then_pause)
 
-file=$(readlink -f "$3")                                            # get path to file
-startline=$(( $( grep -n "<$1>" $file | sed s/':.*'/''/ ) + 1 ))    # extract line where <TAG> appears
+file=$(readlink -f "$3")                                                # get path to file
+startline=$(( $( grep -n "<$1>" $file | sed s/':.*'/''/ ) + 1 ))        # extract line where <TAG> appears
 
-while read -r match; do                                             # loop over every match from subproc (line13)
-    line=$(echo $match | sed s/':.*'//)                             # extract line where '===' appeared
-    [ $line -gt $startline ] && endline=$(( $line - 1 )); break     # search for first '===' line after <TAG>
-done < <(grep -n === $file)                                         # pipe from (subproc) into while loop
-                                                                    # subproc: greps for '===' lines in $file
+while read -r match; do                                                 # loop over every match from subproc (line13)
+    line=$(echo $match | sed s/':.*'//)                                 # extract line where '===' appeared
+    [ $line -gt $startline ] && endline=$(( $line - 1 )); break         # search for first '===' line after <TAG>
+done < <(grep -n === $file)                                             # pipe from (subproc) into while loop
+                                                                        # subproc: greps for '===' lines in $file
 
-./scripts/print_lines_from_file_then_pause $startline $endline $2 $3    # FIXME - check if path to script doesn't break
+print_lines_from_file_then_pause $startline $endline $2 $3              # FIXME - check if path to script doesn't break
