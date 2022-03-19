19b869a4f90eefbf8beceb3de843c16d5dc42ff8
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-19 02:59:33 +0100

    change: hacklab initialization + alpine_create_iso
    hacklab: add database/ to /home/felix/.vscode-server/bin/c722ca6c7eed3d7987c0d5c3df5c45f6b15e77d1/bin/remote-cli:/home/felix/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin, add .CONF reference
    logging: backup file descriptor of stdout for backups if needed downstream
    alpine:  put main task in function, put it in (background-job &)
         ... while informing users about it  (( CHECK TIMING ))

--- backend/log_with_timestamp.sh
+++ backend/log_with_timestamp.sh
@@ -4,8 +4,10 @@
 # instead, use them as logs with timestamps prepended
 # creates a seperate .log file for each script
 
-mkdir -p logs                                                                                                               # check if dir logs/ exists, otherwise create it
-LOGFILE=$( basename "$0" | sed s\#'^'\#'\./logs/'\# | sed s\#'\.sh'\#''\# | sed s\#$\#'\.log'\# )                           # set LOGFILE to ./logs/FILENAME with FILENAME = $0.log
-#LOGFILE=$(echo "$0" | sed s\#'\.sh'\#'\.log'\# | sed s\#'^.*/'\#'/var/log/hacklab/'\# )                                    # try logging in system dir /var/log/ - non-functional without sudo
-#exec &>> "$LOGFILE"                                                                                                        # (too) simple output redirection
-exec > >(gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 ; fflush(stdout) }' | sed s/']'/" - $$]"/ &>> "$LOGFILE") 2>&1   # redirect output with timestamps, flushing stdout to get continuous output
+mkdir -p logs                                                                                   # check if dir logs/ exists, otherwise create it
+LOGFILE=$( basename "$0" | sed s\#^\#'\./logs/'\# | sed s\#'\.sh'\#''\# | sed s\#$\#'\.log'\# ) # set LOGFILE to ./logs/FILENAME with FILENAME = $0.log
+#LOGFILE=$(echo "$0" | sed s\#'\.sh'\#'\.log'\# | sed s\#'^.*/'\#'/var/log/hacklab/'\# )        # try logging in system dir /var/log/ - non-functional without sudo
+#exec &>> "$LOGFILE"                                                                            # (too) simple output redirection
+exec 3>&1                                                                                       # link fd #3 with stdout
+exec > >(gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 ; fflush(stdout) }' \
+| sed s/']'/" - $$]"/ &>> "$LOGFILE") 2>&1                                                      # redirect output with timestamps, flushing stdout to get continuous output
