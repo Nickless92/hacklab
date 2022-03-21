5ddd288c9fbc63fb1e9f5569d961e8f25676c160
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-21 10:55:58 +0100

    change: improve storytime layout
    story.txt:    change tags to "text_XX" and "tweeb_YY"
    story.txt:    move first five tweebs from storytime.sh in here (more appropriate)
    storytime.sh: adjust tags and sleep times
    storytime.sh: fix patchy newlines
    storytime.sh: add wait_for_any_key after last text
    wait_any_key: add newline after "Ok!" prompt
    tmux_create_session: remove "sleep 5" after storytime.sh

--- levels/01/storytime.sh
+++ levels/01/storytime.sh
@@ -2,29 +2,27 @@
 
 clear
 sleep 1s
-print_paragraph_from_file 1 ./levels/01/story.txt 1s                      # type 1-3 line with --- and new line at the end
-wait_for_any_key                                                # call read asleep function
-
-sleep 1s
-echo -ne "\n\n    ---\n\n"
-print_loading_arrows .2s; echo -e "greetings.\n"; sleep 1s
-print_loading_arrows .2s; echo -e "you may not know who i am but you will soon find out.\n"; sleep 1s
-print_loading_arrows .2s; echo -e "expect your current understanding of how the world works to turned on its head.\n"; sleep 1s
-print_loading_arrows .2s; echo -e "expect me.\n"; sleep 1s
-print_loading_arrows .2s; echo -e "()> 0   <-> 2  <3 10)\n"; sleep 1s
-echo -e "\n    ---\n\n"; sleep 1s
-
-print_paragraph_from_file 2 ./levels/01/story.txt 1s
+print_paragraph_from_file text_1 ./levels/01/story.txt 1s
 wait_for_any_key
 
-echo -e "\n"
-print_loading_arrows .5s; print_paragraph_from_file 3 ./levels/01/story.txt 1s
-print_loading_arrows .5s; print_paragraph_from_file 4 ./levels/01/story.txt 1s
-print_loading_arrows .5s; print_paragraph_from_file 5 ./levels/01/story.txt 1s
-print_loading_arrows .5s; print_paragraph_from_file 6 ./levels/01/story.txt 1s
+print_loading_arrows; print_paragraph_from_file tweeb_1 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_2 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_3 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_4 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_5 ./levels/01/story.txt .5
 sleep 1s
 
-print_paragraph_from_file 7 ./levels/01/story.txt 1s
+print_paragraph_from_file text_2 ./levels/01/story.txt 1s
 wait_for_any_key
 
-print_paragraph_from_file 8 ./levels/01/story.txt 1s
+print_loading_arrows; print_paragraph_from_file tweeb_6 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_7 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_8 ./levels/01/story.txt .5
+print_loading_arrows; print_paragraph_from_file tweeb_9 ./levels/01/story.txt .5
+sleep 1s
+
+print_paragraph_from_file text_3 ./levels/01/story.txt 1s
+wait_for_any_key
+
+print_paragraph_from_file text_4 ./levels/01/story.txt 1s
+wait_for_any_key
