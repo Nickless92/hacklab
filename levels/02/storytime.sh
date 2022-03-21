1c8f05f35e92e6fd0ce7e8ed5bb3ae183b71c8f6
Felix Rheinheimer <felix.rheinheimer@hs-worms.de>
2022-03-21 12:08:08 +0100

    fix: levels/02/

--- levels/02/storytime.sh
+++ levels/02/storytime.sh
@@ -2,39 +2,18 @@
 
 clear
 sleep 1s
-print_lines_from_file_then_pause 1 3 1 story.txt                # type 1-3 line with --- and new line at the end
-
-wait_for_any_key                                                # call read asleep function
-
-sleep 1s
-echo -ne "\n\n    ---\n\n"
-loading_arrows .2s; echo -e "greetings.\n"
-sleep 1s
-loading_arrows .2s; echo -e "You may not know what I'm up to, but you'll soon find out.\n"
-sleep 1s
-loading_arrows .2s; echo -e "find what's going on here.\n"
-sleep 1s
-loading_arrows .2s; echo -e "for that you should.\n"
-sleep 1s
-loading_arrows .2s; echo -e "()> 0   <-> 2  <3 10)\n"
-sleep 1s
-echo -e "\n    ---\n\n"
-
-sleep 1s
-print_lines_from_file_then_pause 4 4 1s ./story.txt
-
-sleep 1s
-print_lines_from_file_then_pause 5 5 1s ./story.txt
-
+print_paragraph_from_file text_1 ./story.txt .5
 wait_for_any_key
 
-#loading_arrows .5s; print_lines_from_file_then_pause 8 8 1s ./story.txt
-#loading_arrows .5s; print_lines_from_file_then_pause 9 9 1s ./story.txt
-#loading_arrows .5s; print_lines_from_file_then_pause 10 10 1s ./story.txt 
+print_loading_arrows .2s; print_paragraph_from_file tweeb_1 ./story.txt .5
+print_loading_arrows .2s; print_paragraph_from_file tweeb_2 ./story.txt .5
+print_loading_arrows .2s; print_paragraph_from_file tweeb_3 ./story.txt .5
+print_loading_arrows .2s; print_paragraph_from_file tweeb_4 ./story.txt .5
+print_loading_arrows .2s; print_paragraph_from_file tweeb_5 ./story.txt .5
+wait_for_any_key
 
-#sleep 1s
-#print_lines_from_file_then_pause 11 13 2s ./story.txt
+print_paragraph_from_file text_2 ./story.txt .5
+wait_for_any_key
 
-#wait_for_any_key
-
-#print_lines_from_file_then_pause 14 19 4s ./story.txt
+print_paragraph_from_file text_3 ./story.txt .5
+wait_for_any_key
